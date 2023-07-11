f89ad806e7c1f6c0eb2c7610a08c23a287d737ec
ripjack <thorjackey@gmail.com>
2023-07-11 08:17:59 -0500

    reworks

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,31 @@
 -- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  print("Loading Lazy.nvim...")
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-  print("Done.")
+if not vim.g.vscode then
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    print("Loading Lazy.nvim...")
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+    print("Done.")
+  end
+  vim.opt.rtp:prepend(lazypath)
+  
+  -- load plugins
+  require("plugins")
+  
+  -- load configurations
+  require("configurations")
+  
+  -- setup nvim-cmp
+  require("nvim_cmp")
+  
+  -- setup lspconfig
+  require("nvim_lspconfig")
+  
+  vim.cmd("colorscheme tokyonight")
 end
-vim.opt.rtp:prepend(lazypath)
-
--- load plugins
-require("plugins")
-
--- load configurations
-require("configurations")
-
--- setup nvim-cmp
-require("nvim_cmp")
-
--- setup lspconfig
-require("nvim_lspconfig")
-
-vim.cmd("colorscheme tokyonight")
-
