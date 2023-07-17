35b945bd4fa152a17fa2297714192d034cb654b7
Thorjackie <theranslinkard@gmail.com>
2023-07-17 08:03:01 -0500

    Revert "reworks"
    This reverts commit f89ad806e7c1f6c0eb2c7610a08c23a287d737ec.

--- nvim-coc/init.lua
+++ nvim-coc/init.lua
@@ -1,5 +1,91 @@
-if not vim.g.vscode then
-    require("lazy-bootstrap")
-    require("plugins")
-    require("configurations")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup{
+	{"neoclide/coc.nvim",
+	 branch = "release"
+    },
+	-- nvim-treesitter
+	{
+		'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup{
+                highlight = {enable = true},
+                indent = {enable = true}
+            }
+        end
+	},
+    -- tokyo night theme
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {}
+    -- },
+    {"sainnhe/everforest"},
+    {
+	    "nvim-lualine/lualine.nvim",
+	    event = "VeryLazy",
+	    requires = {
+		    "nvim-tree/nvim-web-devicons"
+	    },
+        config = function ()
+            require("lualine").setup{
+                options = {theme = "everforest"}
+            }
+        end
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("indent_blankline").setup{
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+        		use_treesitter = true,
+        		show_trailing_blankline_indent = false
+            }
+        end
+    },
+    -- barbecue
+    {
+	    "utilyre/barbecue.nvim",
+	    event = "VeryLazy",
+	    dependencies = {
+		    "SmiteshP/nvim-navic",
+		    "nvim-tree/nvim-web-devicons",
+            "folke/tokyonight.nvim"
+	    },
+	    config = function()
+            require("barbecue").setup{
+                theme = "everforest"
+            }
+        end
+    },
+    -- git signs
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup{}
+        end
+    },
+	{
+		'windwp/nvim-autopairs',
+		config = function()
+            require("nvim-autopairs").setup{}
+        end
+	},
+    {"LionC/nest.nvim"}
+ }
+
+ require("configurations")
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,30 @@
 -- bootstrap lazy.nvim
-if not vim.g.vscode then
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    print("Loading Lazy.nvim...")
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-    })
-    print("Done.")
-  end
-  vim.opt.rtp:prepend(lazypath)
-  
-  -- load plugins
-  require("plugins")
-  
-  -- load configurations
-  require("configurations")
-  
-  -- setup nvim-cmp
-  require("nvim_cmp")
-  
-  -- setup lspconfig
-  require("nvim_lspconfig")
-  
-  vim.cmd("colorscheme tokyonight")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  print("Loading Lazy.nvim...")
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+  print("Done.")
 end
+vim.opt.rtp:prepend(lazypath)
+
+-- load plugins
+require("plugins")
+
+-- load configurations
+require("configurations")
+
+-- setup nvim-cmp
+require("nvim_cmp")
+
+-- setup lspconfig
+require("nvim_lspconfig")
+
+vim.cmd("colorscheme tokyonight")
+
