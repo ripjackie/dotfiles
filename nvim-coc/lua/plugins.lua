55ac056db4a7e666fd8e1eb8df402ab90a3bdda2
Thorjackie <theranslinkard@gmail.com>
2023-07-17 08:07:40 -0500

    module styles

---
+++ nvim-coc/lua/plugins.lua
@@ -0,0 +1,77 @@
+require("lazy").setup{
+	{"neoclide/coc.nvim",
+	 branch = "release"
+    },
+	-- nvim-treesitter
+	{
+		'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup{
+                highlight = {enable = true},
+                indent = {enable = true}
+            }
+        end
+	},
+    -- tokyo night theme
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {}
+    -- },
+    {"sainnhe/everforest"},
+    {
+	    "nvim-lualine/lualine.nvim",
+	    event = "VeryLazy",
+	    requires = {
+		    "nvim-tree/nvim-web-devicons"
+	    },
+        config = function ()
+            require("lualine").setup{
+                options = {theme = "everforest"}
+            }
+        end
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("indent_blankline").setup{
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+        		use_treesitter = true,
+        		show_trailing_blankline_indent = false
+            }
+        end
+    },
+    -- barbecue
+    {
+	    "utilyre/barbecue.nvim",
+	    event = "VeryLazy",
+	    dependencies = {
+		    "SmiteshP/nvim-navic",
+		    "nvim-tree/nvim-web-devicons",
+            "folke/tokyonight.nvim"
+	    },
+	    config = function()
+            require("barbecue").setup{
+                theme = "everforest"
+            }
+        end
+    },
+    -- git signs
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup{}
+        end
+    },
+	{
+		'windwp/nvim-autopairs',
+		config = function()
+            require("nvim-autopairs").setup{}
+        end
+	},
+    {"LionC/nest.nvim"}
+ }
