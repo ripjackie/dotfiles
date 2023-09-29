ea89c1a8bdcf9102ccabc4113f4a9927830ad315
ripjackie <theranslinkard@gmail.com>
2023-09-28 23:10:47 -0500

    added neogit

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,10 @@
+return {
+    "Neogit0rg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "sindrets/diffview.nvim"
+    },
+    opts = {}
+}
+
