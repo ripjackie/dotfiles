510dc3aef792a652efad3c0c80973e745123ab24
ripjackie <theranslinkard@gmail.com>
2024-01-10 08:06:45 -0600

    working on mason

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -2,148 +2,188 @@ local vim = vim
 
 local plugins = {
 
-	-- Overrides
+  -- Overrides
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-context",
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"RRethy/nvim-treesitter-endwise",
-			"windwp/nvim-ts-autotag",
-		},
-		opts = {
-			highlight = {
-				enable = true,
-				use_languagetree = true,
-			},
-			indent = { enable = true },
-			autotag = { enable = true },
-			endwise = { enable = true },
-			ensure_installed = {
-				"vim",
-				"vimdoc",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"lua",
-				"go",
-				"python",
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-context",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "RRethy/nvim-treesitter-endwise",
+      "windwp/nvim-ts-autotag",
+    },
+    opts = {
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+      },
+      indent = { enable = true },
+      autotag = { enable = true },
+      endwise = { enable = true },
+      ensure_installed = {
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "lua",
+        "go",
+        "python",
+      },
+    },
+  },
 
-	-- Plugin Replacements
-	--
-	{
-		"williamboman/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"jay-babu/mason-null-ls.nvim",
-			"jay-babu/mason-nvim-dap.nvim",
-		},
-		config = function(_, opts)
-			opts.ensure_installed = {}
+  -- Plugin Replacements
+  --
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {},
+    },
+    config = function(_, opts)
+      local registry = require("mason-registry")
+      dofile(vim.g.base46_cache .. "mason")
+      require("mason").setup(opts)
 
-			require("mason").setup(opts)
-			require("mason-lspconfig").setup({
-				ensure_installed = nil,
-				automatic_installation = true,
-			})
-			require("mason-null-ls").setup({
-				ensure_installed = nil,
-				automatic_installation = true,
-			})
-			require("mason-nvim-dap").setup({
-				ensure_installed = nil,
-				automatic_installation = true,
-			})
-		end,
-	},
+      vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function()
+          local installed = registry.get_installed_package_names()
+          local function hasValue(array, value)
+            for _, v in pairs(array) do
+              if v == value then
+                return true
+              end
+            end
+            return false
+          end
 
-	{
-		"altermo/ultimate-autopair.nvim",
-		lazy = true,
-		event = "InsertEnter",
-		dependencies = {
-			{ "windwp/nvim-autopairs", enabled = false },
-		},
-		opts = {},
-	},
+          for _, value in pairs(opts.ensure_installed) do
+            if not hasValue(registry, value) then
+              local pkg = registry.get_package(value)
+              vim.notify("Automatically Installing " .. pkg.name)
+              pkg:install():once(
+                "closed",
+                vim.schedule_wrap(function()
+                  vim.notify(pkg.name .. " Installed Successfully")
+                end)
+              )
+            end
+          end
+          for _, value in pairs(installed) do
+            if not hasValue(opts.ensure_installed, value) then
+              -- uninstall
+            end
+          end
+        end,
+      })
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"junnplus/lsp-setup.nvim",
-				dependencies = {
-					"folke/neodev.nvim",
-				},
-				config = function()
-					require("custom.configs.lsp-setup")
-				end,
-			},
-		},
-		config = function() end,
-	},
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    lazy = false,
+    opts = {
+      ensure_installed = {
+        -- lua
+        "lua-language-server",
+        "selene",
+        "stylua",
+        -- go
+        "gopls",
+        "gofumpt",
+        "goimports-reviser",
+        "golines",
+        -- python
+        "python-lsp-server",
+        "pyright",
+      },
+      auto_update = true,
+    },
+  },
 
-	-- Custom Plugins
+  {
+    "altermo/ultimate-autopair.nvim",
+    lazy = true,
+    event = "InsertEnter",
+    dependencies = {
+      { "windwp/nvim-autopairs", enabled = false },
+    },
+    opts = {},
+  },
 
-	{
-		"mfussenegger/nvim-dap",
-	},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "junnplus/lsp-setup.nvim",
+        dependencies = {
+          "folke/neodev.nvim",
+        },
+        config = function()
+          require("custom.configs.lsp-setup")
+        end,
+      },
+    },
+    config = function() end,
+  },
 
-	{
-		"nvimtools/none-ls.nvim",
-		ft = { "go", "lua", "python" },
-		opts = function()
-			return require("custom.configs.null-ls")
-		end,
-	},
+  -- Custom Plugins
 
-	{
-		"utilyre/barbecue.nvim",
-		lazy = true,
-		event = "LspAttach",
-		dependencies = {
-			"SmiteshP/nvim-navic",
-			"Nvim-tree/nvim-web-devicons",
-		},
-		opts = {},
-	},
+  {
+    "mfussenegger/nvim-dap",
+  },
 
-	{
-		"j-hui/fidget.nvim",
-		lazy = true,
-		event = "LspAttach",
-		tag = "v1.1.0",
-		opts = {},
-	},
+  {
+    "nvimtools/none-ls.nvim",
+    ft = { "go", "lua", "python" },
+    opts = function()
+      return require("custom.configs.null-ls")
+    end,
+  },
 
-	{
-		"m-demare/hlargs.nvim",
-		lazy = true,
-		event = "LspAttach",
-		opts = {},
-	},
+  {
+    "utilyre/barbecue.nvim",
+    lazy = true,
+    event = "LspAttach",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "Nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+  },
 
-	{
-		"kylechui/nvim-surround",
-		lazy = true,
-		event = "InsertEnter",
-		version = "*",
-		opts = {},
-	},
+  {
+    "j-hui/fidget.nvim",
+    lazy = true,
+    event = "LspAttach",
+    tag = "v1.1.0",
+    opts = {},
+  },
 
-	{
-		"folke/trouble.nvim",
-		lazy = true,
-		cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {},
-	},
+  {
+    "m-demare/hlargs.nvim",
+    lazy = true,
+    event = "LspAttach",
+    opts = {},
+  },
+
+  {
+    "kylechui/nvim-surround",
+    lazy = true,
+    event = "InsertEnter",
+    version = "*",
+    opts = {},
+  },
+
+  {
+    "folke/trouble.nvim",
+    lazy = true,
+    cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+  },
 }
 
 return plugins
