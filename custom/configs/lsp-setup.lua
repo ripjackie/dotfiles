5e87b2f643d92cc4d2a8c1d4d374ac3d1ea808f1
ripjackie <theranslinkard@gmail.com>
2024-01-09 22:00:26 -0600

    message

--- custom/configs/lsp-setup.lua
+++ custom/configs/lsp-setup.lua
@@ -1,29 +1,68 @@
-local config = require "plugins.configs.lspconfig"
+local config = require("plugins.configs.lspconfig")
 
-require("neodev").setup {
-  library = {
-    enabled = true,
-    types = true,
-    runtime = true,
-    plugins = true,
-  },
-}
+require("neodev").setup({
+	library = {
+		enabled = true,
+		types = true,
+		runtime = true,
+		plugins = true,
+	},
+})
 
-require("lsp-setup").setup {
-  default_mappings = false,
-  inlay_hints = { enabled = true },
-  on_attach = config.on_attach,
-  capabilities = config.capabilities,
+require("lsp-setup").setup({
+	default_mappings = false,
+	inlay_hints = { enabled = true },
+	on_attach = config.on_attach,
+	capabilities = config.capabilities,
 
-  servers = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-    pyright = {},
-    pylsp = {},
-  },
-}
+	servers = {
+		lua_ls = {
+			settings = {
+				Lua = {
+					format = {
+						enable = true,
+						defaultConfig = {
+							indent_style = "space",
+							indent_size = "4",
+						},
+					},
+					runtime = {
+						version = "LuaJIT",
+					},
+					diagnostics = {
+						globals = { "vim" },
+					},
+					hint = {
+						enable = true,
+						arrayIndex = "Auto",
+						await = true,
+						paramName = "All",
+						paramType = true,
+						semicolon = "SameLine",
+						setType = false,
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+							[vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types"] = true,
+							[vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
+						},
+						maxPreload = 10000,
+						preloadFileSize = 1000,
+					},
+				},
+			},
+		},
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
+		},
+		pyright = {},
+		pylsp = {},
+	},
+})
--- custom/plugins.lua
+++ custom/plugins.lua
@@ -1,138 +1,149 @@
+local vim = vim
+
 local plugins = {
 
-  -- Overrides
-  {
-    "williamboman/mason.nvim",
-    opts = function()
-      local opts = require "plugins.configs.mason"
-      opts.ensure_installed = {
-        -- lua
-        "lua-language-server",
-        "selene",
-        "stylua",
-        -- go
-        "gopls",
-        "golangci-lint",
-        "gofumpt",
-        "goimports-reviser",
-        -- python
-        "python-lsp-server",
-        "pyright",
-      }
-      return opts
-    end,
-  },
+	-- Overrides
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-context",
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "RRethy/nvim-treesitter-endwise",
-      "windwp/nvim-ts-autotag",
-    },
-    opts = {
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-      },
-      indent = { enable = true },
-      autotag = { enable = true },
-      endwise = { enable = true },
-      ensure_installed = {
-        "vim",
-        "vimdoc",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "go",
-        "python",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"RRethy/nvim-treesitter-endwise",
+			"windwp/nvim-ts-autotag",
+		},
+		opts = {
+			highlight = {
+				enable = true,
+				use_languagetree = true,
+			},
+			indent = { enable = true },
+			autotag = { enable = true },
+			endwise = { enable = true },
+			ensure_installed = {
+				"vim",
+				"vimdoc",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"go",
+				"python",
+			},
+		},
+	},
 
-  -- Plugin Replacements
+	-- Plugin Replacements
+	--
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"jay-babu/mason-null-ls.nvim",
+			"jay-babu/mason-nvim-dap.nvim",
+		},
+		config = function(_, opts)
+			opts.ensure_installed = {}
 
-  {
-    "altermo/ultimate-autopair.nvim",
-    lazy = true,
-    event = "InsertEnter",
-    dependencies = {
-      { "windwp/nvim-autopairs", enabled = false },
-    },
-    opts = {},
-  },
+			require("mason").setup(opts)
+			require("mason-lspconfig").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+			require("mason-nvim-dap").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "junnplus/lsp-setup.nvim",
-        dependencies = {
-          "folke/neodev.nvim",
-        },
-        config = function()
-          require "custom.configs.lsp-setup"
-        end,
-      },
-    },
-    config = function() end,
-  },
+	{
+		"altermo/ultimate-autopair.nvim",
+		lazy = true,
+		event = "InsertEnter",
+		dependencies = {
+			{ "windwp/nvim-autopairs", enabled = false },
+		},
+		opts = {},
+	},
 
-  -- Custom Plugins
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"junnplus/lsp-setup.nvim",
+				dependencies = {
+					"folke/neodev.nvim",
+				},
+				config = function()
+					require("custom.configs.lsp-setup")
+				end,
+			},
+		},
+		config = function() end,
+	},
 
-  {
-    "nvimtools/none-ls.nvim",
-    ft = { "go", "lua", "python" },
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
+	-- Custom Plugins
 
-  {
-    "utilyre/barbecue.nvim",
-    lazy = true,
-    event = "LspAttach",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "Nvim-tree/nvim-web-devicons",
-    },
-    opts = {},
-  },
+	{
+		"mfussenegger/nvim-dap",
+	},
 
-  {
-    "j-hui/fidget.nvim",
-    lazy = true,
-    event = "LspAttach",
-    tag = "v1.1.0",
-    opts = {},
-  },
+	{
+		"nvimtools/none-ls.nvim",
+		ft = { "go", "lua", "python" },
+		opts = function()
+			return require("custom.configs.null-ls")
+		end,
+	},
 
-  {
-    "m-demare/hlargs.nvim",
-    lazy = true,
-    event = "LspAttach",
-    opts = {},
-  },
+	{
+		"utilyre/barbecue.nvim",
+		lazy = true,
+		event = "LspAttach",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"Nvim-tree/nvim-web-devicons",
+		},
+		opts = {},
+	},
 
-  {
-    "kylechui/nvim-surround",
-    lazy = true,
-    event = "InsertEnter",
-    version = "*",
-    opts = {},
-  },
+	{
+		"j-hui/fidget.nvim",
+		lazy = true,
+		event = "LspAttach",
+		tag = "v1.1.0",
+		opts = {},
+	},
 
-  {
-    "folke/trouble.nvim",
-    lazy = true,
-    cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {},
-  },
+	{
+		"m-demare/hlargs.nvim",
+		lazy = true,
+		event = "LspAttach",
+		opts = {},
+	},
+
+	{
+		"kylechui/nvim-surround",
+		lazy = true,
+		event = "InsertEnter",
+		version = "*",
+		opts = {},
+	},
+
+	{
+		"folke/trouble.nvim",
+		lazy = true,
+		cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {},
+	},
 }
 
 return plugins
